94e92cefd148fa70cc079d2fdb95252ccf091733
Nicoara Sebastian <sebastian.nicoara@corebuild.eu>
2019-11-19 15:38:45 +0200

    chore: remove build output files

--- ch5-utilities-webpack-plugin/build/@types/WebpackCh5DeployPlugin.d.ts
+++
@@ -1,7 +0,0 @@
-import { Compiler, Plugin } from "webpack";
-export declare class WebpackCh5DeployPlugin implements Plugin {
-    apply(compiler: Compiler): void;
-    private static validateConfig;
-    private static getArchivePath;
-    private static getDeviceType;
-}
